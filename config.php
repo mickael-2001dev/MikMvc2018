58144b0ec14c5690fe1fdf17232404cff5c379b7
Mickael Souza <mickael.souza.if@gmail.com>
2018-03-15 16:04:55 -0300

    Update config.php

--- config.php
+++ config.php
@@ -13,17 +13,4 @@ if($_SERVER['HTTP_HOST'] == 'localhost') {
 	$config->dbname ='mvc-mika';
 	$config->host = 'localhost';
 	$config->drive = 'mysql';
-} /*else {
-	$config->url = 'http://200.132.49.218'.$config->base_url;	
-	$config->dbuser = '2017_mickaelbraz'; 
-	$config->dbpassword = 'micka13022001el'; 
-	$config->dbname ='2017_mickaelbraz';
-	$config->host = '127.0.0.1';
-	$config->drive = 'mysql';
-}*/
-
-
-//Login: adminMK
-//Senha: admin1991
-//Para testar o mailer subistitua o meu email pelo seu ;)
-
+} 
