e8e3c861c31e659f938f41af765ee1e7973a077c
Mickael Souza <mickael.souza.if@gmail.com>
2018-03-15 16:06:25 -0300

    Update View.php

--- views/View.php
+++ views/View.php
@@ -7,7 +7,8 @@ class View {
     private $asset;
     private $template;
 
-    public function __construct() {
+    public function __construct() 
+    {
         include 'config.php';
         if ($config) {
             $this->base_url = $config->base_url;
@@ -18,22 +19,26 @@ class View {
         }
     }
     
-    public function index(){
+    public function index()
+    {
         $this->location($this->url);
     }
 
-    public function load($page, $data = null) {
+    public function load($page, $data = null) 
+    {
         include_once "views/templates/" . $this->template . "/$page.php";
     }
 
-    public function setTemplate($template) {
+    public function setTemplate($template) 
+    {
         include 'config.php';
         $this->asset = $config->asset;
         $this->template = $template;
          $this->asset.=$this->template . "/";
     }
     
-    public function location($url){
+    public function location($url)
+    {
          header("Location: $url");
     }
 
