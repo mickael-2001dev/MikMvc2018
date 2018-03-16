642ba3b086c7dff0e9f9fb14756de61e62b435f3
Mickael Souza <mickael.souza.if@gmail.com>
2018-03-16 15:08:16 -0300

    Update Model.php

--- models/Model.php
+++ models/Model.php
@@ -2,21 +2,10 @@
 namespace models;
 use \PDO;
 use PDOException;
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- * Description of Model
- *
- * @author echoes
- */
 class Model 
 {
-    //put your code here
-   
+  
     private $connectionString;
     private $user;
     private $password;
